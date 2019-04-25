7d481d9bf5c243fd33dc80fa3d34967603aeaf59
zhaoshen <zhaoshen@autoai.com>
2019-04-25 14:16:22 +0800

    测试冲突 V.0.1

--- src/main/java/com/autoai/importDemo/A_test01.java
+++ src/main/java/com/autoai/importDemo/A_test01.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class test {
+public class A_test01 {
 
     @RequestMapping("/test")
     public String test1() {
