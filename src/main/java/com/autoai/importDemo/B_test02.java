fc0b1880767067f04b6ba50c310fd3b5278d2af3
sn-zhao <734688897@qq.com>
2019-04-25 14:24:38 +0800

    测试merge冲突

--- src/main/java/com/autoai/importDemo/B_test02.java
+++ src/main/java/com/autoai/importDemo/B_test02.java
@@ -1,1 +1,14 @@
+package com.autoai.importDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class B_test02 {
+
+    @RequestMapping("/b/test02")
+    public String test1() {
+        return "b_test03";
+    }
+}
 
